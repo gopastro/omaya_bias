c68a4d56665aab3fe3ceb06a4671e69a461ca1d7
Selah Finkelstein <selahfinkelstein@github.com>
2019-06-27 16:19:53 -0400

    edits

--- omayabias/utils/norm_state_resistance.py
+++ omayabias/utils/norm_state_resistance.py
@@ -1,5 +1,5 @@
 import numpy as np
-def norm_state_res(data_array):
+def norm_state_res(data_array, voltage):
     '''
     Performs line fit to calculate normal state resistance of SIS junctions.
     Averages the fit from the positive and negative voltages of the IV curve
@@ -11,14 +11,14 @@ def norm_state_res(data_array):
            average slope, average slope error
            average y-intercept, average y-intercept error
     ''' 
-    subset_pos = data_array[data_array.Vs >=0.015]
+    subset_pos = data_array[data_array.Vs >=voltage]
     x_pos=np.array(subset_pos['Vs'])
     y_pos=np.array(subset_pos['Is'])
     fit_pos, err_pos=np.polyfit(x_neg, y_neg, 1, cov=True)
             
     #Negative subset voltages <= -0.015 V
 
-    subset_neg = data_array[data_array.Vs <= -0.015]
+    subset_neg = data_array[data_array.Vs <= -voltage]
     x_neg = np.array(subset_neg['Vs'])
     y_neg = np.array(subset_neg['Is'])
     fit_neg,err_neg=np.polyfit(x_neg,y_neg,1,cov=True)
@@ -30,5 +30,5 @@ def norm_state_res(data_array):
     resistance = 1/avg_slope
     resistance_err=avg_slope_err
     #print(avg_slope_err,avg_int_err,resistance, resistance_err)
-   # return [[resistance, resistance_err],[avg_slope,avg_slope_err],[avg_int, avg_int_err]]
-    return fit_pos,err_pos
+    return [[resistance, resistance_err],[avg_slope,avg_slope_err],[avg_int, avg_int_err]]
+    #return fit_pos,err_pos
