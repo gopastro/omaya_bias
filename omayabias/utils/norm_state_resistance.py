a378c1beadf0cdfeed1eb6f851eb331b0008d09c
selahfinkelstein <44846980+selahfinkelstein@users.noreply.github.com>
2019-07-16 14:59:53 -0400

    Update norm_state_resistance.py

--- omayabias/utils/norm_state_resistance.py
+++ omayabias/utils/norm_state_resistance.py
@@ -6,34 +6,31 @@ def norm_state_res(data_array, vmin, vmax):
     input:
         data_array: array containing sensed voltage and current measurements
     returns:
-       3x2 matrix:
-           resistance, resistance error
-           average slope, average slope error
-           average y-intercept, average y-intercept error
+       params: [slope, intercept]
+       err: covariance matrix for fit
+       x,y: points used in fitting
     ''' 
-    #print(data_array)
-    pre_pos = data_array[data_array.Vs <= vmax ]
-    subset_pos = pre_pos[vmin <= pre_pos.Vs]
-    x_pos=np.array(subset_pos['Vs'])
-    y_pos=np.array(subset_pos['Is'])
-    fit_pos, err_pos=np.polyfit(x_pos, y_pos, 1, cov=True)
-            
-    #Negative subset voltages <= -0.015 V
+    pre_pos = data_array[data_array.Vs <= vmax]
     pre_neg = data_array[data_array.Vs >= -vmax]
+
+    subset_pos = pre_pos[vmin <= pre_pos.Vs]
     subset_neg = pre_neg[pre_neg.Vs <= -vmin]
+
+    x_pos = np.array(subset_pos['Vs'])
+    y_pos = np.array(subset_pos['Is'])
+
     x_neg = np.array(subset_neg['Vs'])
     y_neg = np.array(subset_neg['Is'])
-    fit_neg,err_neg=np.polyfit(x_neg,y_neg,1,cov=True)
 
-    avg_slope = (fit_pos[0]+fit_neg[0])/2
-    avg_int = (fit_pos[1]+fit_neg[1])/2
-    avg_slope_err=np.sqrt(err_pos[0,0]+err_neg[0,0])
-    avg_int_err=np.sqrt(err_pos[1,1]+err_neg[1,1])
-    resistance = 1/avg_slope
-    resistance_err=avg_slope_err
-    #print(avg_slope_err,avg_int_err,resistance, resistance_err)
-    return [[resistance, resistance_err],[avg_slope,avg_slope_err],[avg_int, avg_int_err]]
-    #return fit_pos,err_pos
+    x = np.concatenate((x_neg, x_pos))
+    y = np.concatenate((y_neg, y_pos))
+    #fit_pos, err_pos=np.polyfit(x_pos, y_pos, 1, cov=True)
+    
+    params, err = np.polyfit(x_pos, y_pos, 1, cov=True)
 
+    #y_fit = fit_func(x, slope, intercept)
 
+    #return params,cov
+
+    return [params, err,x,y]
     
